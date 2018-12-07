12bcff25c27e7d895a5143573d6fcde74c5fafcc
Kevin Tabak <kevin_tabak@outlook.com>
2018-12-08 00:28:46 +0100

    Day 2 part 2
    Added part 2 of day 2.

--- src/D2_InventoryManagementSystem.java
+++ src/D2_InventoryManagementSystem.java
@@ -3,11 +3,13 @@ import java.nio.file.*;
 import java.util.*;
 
 public class D2_InventoryManagementSystem {
+    //TODO: Commenting
     private List<String> lines;
 
     public static void main(String[] args) {
         D2_InventoryManagementSystem d2 = new D2_InventoryManagementSystem("assets", "Day2-Input.txt");
         System.out.println("Answer to part 1 is: "+d2.getChecksum());
+        System.out.println("Answer to part 2 is: "+d2.compareLines());
     }
 
     /**
@@ -47,16 +49,55 @@ public class D2_InventoryManagementSystem {
     }
 
     private boolean hasAmount(String line, int amount){
-        String[] letters = line.split("");
         boolean hasAmount = false;
-        List<String> asList = Arrays.asList(letters);
+        List<String> asList = Arrays.asList(line.split(""));
         HashSet<String> lettersSet = new HashSet<>(asList);
+
         for(String s: lettersSet){
             if(Collections.frequency(asList,s) == amount){
                 hasAmount = true;
             }
         }
+
         return hasAmount;
     }
 
+    private String compareLines(){
+        StringBuilder toReturn = new StringBuilder();
+
+        for (int i = 0; i < lines.size(); i++){
+            toReturn.append(compareLine(i));
+        }
+
+        return toReturn.toString();
+    }
+
+    /**
+     *
+     * @param index index of the line to check
+     * @return The line who differ by 1 letter and remove that letter.
+     */
+    private String compareLine(int index) {
+        String line = lines.get(index);
+        for (String toCompare : lines) {
+            int amountDiffer = 0;
+            Integer removeIndex = null;
+
+            for (int i = 0; i < line.length(); i++){
+                if (line.charAt(i) != toCompare.charAt(i)){
+                    amountDiffer++;
+                    removeIndex = i;
+                }
+            }
+
+            if (amountDiffer == 1){
+                StringBuilder sb = new StringBuilder(toCompare);
+                sb.deleteCharAt(removeIndex);
+                toCompare = sb.toString();
+                lines.remove(index);
+                return toCompare;
+            }
+        }
+        return "";
+    }
 }
